815d697be8ed100c7ebb648650f4622e714d14ed
lunaroyster <oneinaquadrillion@live.com>
2020-11-07 20:18:33 +0000

    debug

--- dist/index.js
+++ dist/index.js
@@ -7,10 +7,43 @@ module.exports =
 
 const core = __webpack_require__(201);
 const github = __webpack_require__(283);
+const fetch = __webpack_require__(341);
 
-console.log('test');
-core.setOutput("test", Date.now().toString());
+async function discord(webhook, args) {
+  try {
+    const res = await fetch(webhook, {
+      method: 'POST',
+      body: JSON.stringify({
+        username: 'GitHub',
+        avatar_url: 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png',
+        ...args,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+    return res;
+  } catch (e) {
+    console.log(e)
+  }
+}
 
+async function debug(webhook) {
+  await discord(webhook, {
+    content: JSON.stringify(github),
+  })
+}
+
+async function main() {
+  try {
+    const discordWebhook = core.getInput('discord-webhook');
+    await debug(discordWebhook);
+  } catch (e) {
+    core.setFailed(e.message);
+  }
+}
+
+main();
 
 /***/ }),
 
--- index.js
+++ index.js
@@ -1,5 +1,39 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fetch = require('node-fetch');
 
-console.log('test');
-core.setOutput("test", Date.now().toString());
+async function discord(webhook, args) {
+  try {
+    const res = await fetch(webhook, {
+      method: 'POST',
+      body: JSON.stringify({
+        username: 'GitHub',
+        avatar_url: 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png',
+        ...args,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+    return res;
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+async function debug(webhook) {
+  await discord(webhook, {
+    content: JSON.stringify(github),
+  })
+}
+
+async function main() {
+  try {
+    const discordWebhook = core.getInput('discord-webhook');
+    await debug(discordWebhook);
+  } catch (e) {
+    core.setFailed(e.message);
+  }
+}
+
+main();
